94b3b4621be520557531cbab0f46f232d1058bb6
Stephen <skaranja654@gmail.com>
2023-06-10 13:20:36 +0300

    feat:Background

--- src/Background.js
+++ src/Background.js
@@ -1,5 +1,14 @@
+import React from "react";
+
 function Background() {
-  return "This is a test";
+  return (
+    <div>
+      <img
+        src="/home/steve/Personal Projects/Portfolio/my-app/Images/maurits-bausenhart--zxeFLnmnkQ-unsplash.jpg"
+        alt="Background"
+      />
+    </div>
+  );
 }
 
 export default Background;
